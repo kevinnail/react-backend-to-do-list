5cfab97f565d352a4c2714be6e89cb454a92a9b1
kevinnail <fumalicious@gmail.com>
2022-12-02 15:14:18 -0800

    sql update

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users2;
+DROP TABLE IF EXISTS todos;
 
 CREATE TABLE users2 (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -9,3 +10,11 @@ CREATE TABLE users2 (
   first_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL
 );
+
+CREATE TABLE todos(
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id VARCHAR NOT NULL,
+  task VARCHAR NOT NULL,
+  complete BOOLEAN NOT NULL, 
+  FOREIGN KEY (user_id) REFERENCES users2(id)
+  )
