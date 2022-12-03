88d7e26d924dc67f1c2edf20af45d112a5a0038d
kevinnail <fumalicious@gmail.com>
2022-12-02 17:27:30 -0800

    sql adjusted

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,11 +1,11 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users2;
-DROP TABLE IF EXISTS todos;
+DROP TABLE IF EXISTS users2 CASCADE;
+DROP TABLE IF EXISTS todos CASCADE;
 
 CREATE TABLE users2 (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  email VARCHAR,
+  email  VARCHAR UNIQUE,
   password_hash VARCHAR NOT NULL,
   first_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL
@@ -13,8 +13,9 @@ CREATE TABLE users2 (
 
 CREATE TABLE todos(
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  user_id VARCHAR NOT NULL,
+  user_id BIGINT NOT NULL,
   task VARCHAR NOT NULL,
-  complete BOOLEAN NOT NULL, 
+  completed BOOLEAN NOT NULL, 
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   FOREIGN KEY (user_id) REFERENCES users2(id)
   )
